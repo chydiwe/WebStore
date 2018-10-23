25502befded43d58b16813acd0c1b9ad3e98ad2d
usususruss <ususus_russ@mail.ru>
2018-10-23 10:18:57 +0300

    Registration

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,14 +7,14 @@ import {history, store} from './store/configureStore'
 import {Route, Switch} from 'react-router-dom'
 import {ConnectedRouter,} from 'react-router-redux'
 import ConfirmMail from "./containers/confirmMail";
-import FormRegister from './components/register'
+import Register from "./components/registerForm";
 ReactDOM.render(
     <Provider store={store}>
         <ConnectedRouter history={history}>
             <Switch>
                 <Route exact path='/' component={Main}/>
                 <Route path='/confirming' component={ConfirmMail}/>
-                <Route path='/register' component={FormRegister}/>
+                <Route path='/register' component={Register}/>
             </Switch>
         </ConnectedRouter>
     </Provider>,
