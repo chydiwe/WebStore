0e197c5a4bbf64094aba82900d2ff1071885f7d6
Well <wellusha@gmail.com>
2018-11-09 10:02:44 +0300

    Minor change to name of path

--- backend/src/main/java/com/jackass/RestAPI/controller/OrderStatusController.java
+++ backend/src/main/java/com/jackass/RestAPI/controller/OrderStatusController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Set;
 
 @RestController
-@RequestMapping("/api/orders/status")
+@RequestMapping("/api/orders/statuses")
 @CrossOrigin(origins = "http://localhost:3000")
 public class OrderStatusController {
 
