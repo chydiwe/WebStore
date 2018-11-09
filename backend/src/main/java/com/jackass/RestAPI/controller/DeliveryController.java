6cf3d408f4520aac27d198cc02431e14df9516a0
Well <wellusha@gmail.com>
2018-11-09 10:08:43 +0300

    Minor change to name of path

--- backend/src/main/java/com/jackass/RestAPI/controller/DeliveryController.java
+++ backend/src/main/java/com/jackass/RestAPI/controller/DeliveryController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Set;
 
 @RestController
-@RequestMapping("/api/deliveries")
+@RequestMapping("/api/orders/deliveries")
 @CrossOrigin(origins = "http://localhost:3000")
 public class DeliveryController {
 
--- backend/src/main/java/com/jackass/RestAPI/controller/PaymentController.java
+++ backend/src/main/java/com/jackass/RestAPI/controller/PaymentController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Set;
 
 @RestController
-@RequestMapping("/api/payments")
+@RequestMapping("/api/orders/payments")
 @CrossOrigin(origins = "http://localhost:3000")
 public class PaymentController {
 
