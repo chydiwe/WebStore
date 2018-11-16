64d79d211e503119e240deb3b5aba7bd4af7e947
usususruss <ususus_russ@mail.ru>
2018-11-17 01:57:41 +0300

    starting making react from html..

--- frontend/src/components/OrderPage.js
+++ frontend/src/components/OrderPage.js
@@ -1,10 +1,93 @@
 import React, {Component} from 'react'
 import "./css/OrderPage.css"
-// import minus from './img/minus.png'
+import notFound from "./img/notfound.png";
+
+const Minus = () =>
+    <svg xmlns="http://www.w3.org/2000/svg" version="1.1" x="0px" y="0px" viewBox="0 0 52 52" width="40px" height="40px">
+        <g>
+            <path d="M26,0C11.664,0,0,11.663,0,26s11.664,26,26,26s26-11.663,26-26S40.336,0,26,0z M26,50C12.767,50,2,39.233,2,26   S12.767,2,26,2s24,10.767,24,24S39.233,50,26,50z" fill="#bfbfbf"/>
+            <path d="M39,25H13c-0.552,0-1,0.447-1,1s0.448,1,1,1h26c0.552,0,1-0.447,1-1S39.552,25,39,25z" fill="#bfbfbf"/>
+        </g>
+    </svg>
+
+const Plus = () =>
+    <svg xmlns="http://www.w3.org/2000/svg" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 52 52" width="40px" height="40px">
+        <g>
+            <path d="M26,0C11.664,0,0,11.663,0,26s11.664,26,26,26s26-11.663,26-26S40.336,0,26,0z M26,50C12.767,50,2,39.233,2,26   S12.767,2,26,2s24,10.767,24,24S39.233,50,26,50z" fill="#bfbfbf"/>
+            <path d="M38.5,25H27V14c0-0.553-0.448-1-1-1s-1,0.447-1,1v11H13.5c-0.552,0-1,0.447-1,1s0.448,1,1,1H25v12c0,0.553,0.448,1,1,1   s1-0.447,1-1V27h11.5c0.552,0,1-0.447,1-1S39.052,25,38.5,25z" fill="#bfbfbf"/>
+        </g>
+    </svg>
+
+
+class ShoppingCartItem extends Component {
+    constructor(props){
+        super(props);
+
+        /* вызов метода запроса полной инфы о товаре по id, сохранение результата в переменную productInfo */
+        /* let productInfo = ... */
+        // const {id, name, manufacturer, cost, images} = productInfo;
+
+        this.state = {
+            quantity: 1,
+            itemCountSum: 100 /* props.cost */
+        };
+        this.quantityAdd = this.quantityAdd.bind(this);
+        this.quantityReduce = this.quantityReduce.bind(this);
+    }
+
+    quantityAdd(){
+        if (this.state.quantity !== 50) {
+            this.setState({
+                quantity: ++this.state.quantity,
+                itemCountSum: this.state.quantity * 100 /* cost */
+            });
+        }
+    }
+
+    quantityReduce(){
+        if (this.state.quantity !== 1) {
+            this.setState({
+                quantity: --this.state.quantity,
+                itemCountSum: this.state.quantity * 100 /* cost */
+            });
+        }
+    }
+
+    render() { /* {item,i} */
+        return(
+            <tr className="item"> {/* key={i} */}
+                <td className="image"><img src={notFound} alt=""/></td>
+                <td className="description">
+                    <span>Our Legacy</span>
+                    <span>Brushed Scarf</span>
+                    <span>Brown</span>
+                </td>
+
+                <td className="quantity">
+                    <div onClick={this.quantityAdd}>
+                        <Plus/>
+                    </div>
+                    <p>{this.state.quantity}</p>
+                    <div onClick={this.quantityReduce}>
+                        <Minus />
+                    </div>
+                </td>
+
+                <td className="total-price">{ this.state.itemCountSum } руб.</td>
+
+                <td className="delete-btn">
+                    <img src="http://s1.iconbird.com/ico/0612/GooglePlusInterfaceIcons/w18h181338911473cross.png" alt=""/>
+                </td>
+            </tr>
+        );
+    }
+}
+
+
 
 export default class OrderPage extends Component{
 
-    render(productIdList) {
+    render() {
         return (
             <div className="order-container">
                 <div>
@@ -13,88 +96,20 @@ export default class OrderPage extends Component{
                         Корзина
                     </div>
                     <table>
-
                         <tbody>
-                        <tr className="hat">
-                            <td>Продукт</td>
-                            <td>Описание</td>
-                            <td>Количество</td>
-                            <td>Цена</td>
-                            <td></td>
-                        </tr>
+                            <tr className="hat">
+                                <td>Продукт</td>
+                                <td>Описание</td>
+                                <td>Количество</td>
+                                <td>Цена</td>
+                                <td></td>
+                            </tr>
 
-                        <tr className="item">
-                            <td className="image"><img src="https://p.fast.ulmart.ru/p/small/427/42717/4271777_1s.jpg" alt=""/></td>
-                            <td className="description">
-                                <span>Common Projects</span>
-                                <span>Bball High</span>
-                                <span>White</span>
-                            </td>
-                            <td className="quantity">
-                                <button className="plus-btn" type="button" name="button">
-                                    <img src="http://s1.iconbird.com/ico/0912/fugue/w16h161349012884plus.png" alt=""/>
-                                </button>
-                                <input type="text" name="name" value="1"/>
-                                    <button className="minus-btn" type="button" name="button">
-                                        <img src="http://s1.iconbird.com/ico/0912/fugue/w16h161349012761minus.png" alt=""/>
-                                    </button>
-                            </td>
-                            <td className="total-price">$549</td>
-                            <td className="delete-btn">
-                                <img src="http://s1.iconbird.com/ico/0612/GooglePlusInterfaceIcons/w18h181338911473cross.png" alt=""/>
-                            </td>
-                        </tr>
+                            <ShoppingCartItem />
 
-                        <tr className="item">
-                            <td className="image"><img src="https://p.fast.ulmart.ru/p/small/427/42717/4271777_1s.jpg" alt=""/></td>
-                            <td className="description">
-                                <span>Maison Margiela</span>
-                                <span>Future Sneakers</span>
-                                <span>White</span>
-                            </td>
-                            <td className="quantity">
-                                <button className="plus-btn" type="button" name="button">
-                                    <img src="http://s1.iconbird.com/ico/0912/fugue/w16h161349012884plus.png" alt="" />
-                                </button>
-                                <input type="text" name="name" value="1"/>
-                                    <button className="minus-btn" type="button" name="button">
-                                        <img src="http://s1.iconbird.com/ico/0912/fugue/w16h161349012761minus.png" alt="" />
-                                    </button>
-                            </td>
-                            <td className="total-price">$870</td>
-                            <td className="delete-btn">
-                                <img src="http://s1.iconbird.com/ico/0612/GooglePlusInterfaceIcons/w18h181338911473cross.png" alt=""/>
-                            </td>
 
-                        </tr>
-
-                        <tr className="item">
-                            <td className="image"><img src="https://p.fast.ulmart.ru/p/small/427/42717/4271777_1s.jpg" alt=""/></td>
-                            <td className="description">
-                                <span>Our Legacy</span>
-                                <span>Brushed Scarf</span>
-                                <span>Brown</span>
-                            </td>
-                            <td className="quantity">
-                                <button className="plus-btn" type="button" name="button">
-                                    <img src="http://s1.iconbird.com/ico/0912/fugue/w16h161349012884plus.png" alt=""/>
-                                </button>
-                                <input type="text" name="name" value="1"/>
-                                    <button className="minus-btn" type="button" name="button">
-                                        <img src="http://s1.iconbird.com/ico/0912/fugue/w16h161349012761minus.png" alt=""/>
-                                    </button>
-                            </td>
-                            <td className="total-price">$349</td>
-                            <td className="delete-btn">
-                                <img src="http://s1.iconbird.com/ico/0612/GooglePlusInterfaceIcons/w18h181338911473cross.png" alt=""/>
-                            </td>
-
-                        </tr>
                         </tbody>
                     </table>
-
-
-
 
                     <div className="order-accept-block">
                         <div className="float-right">
@@ -105,9 +120,6 @@ export default class OrderPage extends Component{
                         </div>
                         <div className="clear-float"></div>
                     </div>
-
-
-
 
                 </div>
             </div>
