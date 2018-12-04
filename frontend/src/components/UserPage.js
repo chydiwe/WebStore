1f2f4c463c1e59dbfe7599261ad89778c63c440c
Skier97 <anton_ig@inbox.ru>
2018-12-04 23:49:27 +0300

    beta-version user page

--- frontend/src/components/UserPage.js
+++ frontend/src/components/UserPage.js
@@ -5,7 +5,7 @@ import notFound from "./img/notFound_mini.png";
 const Data =
     {
         image: notFound,
-        surname: "Веселый",
+        surname: "Обосрись",
         name: "Васян",
         patronymic: "Улдарович",
         email: "vasyyyan@mail.ru",
@@ -58,62 +58,102 @@ const UserItem = ({item}) =>
 export default class UserPage extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            user:{
+                id:"1",
+                email: "vasyyyan@mail.ru",
+                country:"Россия",
+                city:"Баянск",
+                name: "Васян",
+                surname: "Обосрись",
+                patronymic: "Улдарович",
+                phoneNumber: "88005553535"
+            },
+            formDisabled:true,
+            formDisabled_True:true
+
+        };
 
     }
 
+
+    setEditable = (event) => {
+        document.getElementById("change").style.display="none";
+        document.getElementById("save").style.display="block";
+        this.setState({
+            formDisabled: false
+        })
+    }
+    handleInput = (event)=>{
+        this.setState({
+            user: {
+                ...this.state.user,
+                [event.target.id]: event.target.value
+            }
+        })
+    }
+
+
+    updateUserInfo = () => {
+        console.dir(this.state)
+        this.setState({
+            formDisabled: true
+        })
+        document.getElementById("change").style.display="block";
+        document.getElementById("save").style.display="none";
+    }
 
     render() {
 
         return (
             <div className="container">
-                <div id="main">
+                <div>
                     <div className="panel-body">
                         <div className="text-center" id="author">
                             <img src={Data.image} alt=""/>
                             <h3> {Data.name} </h3>
+                            <button className="button">Загрузить фото</button>
                         </div>
-
-
                         <table className='infoUser'>
                             <h4 className="text-center"> Личная информация </h4>
                             <tbody>
                             <tr>
-                                <td className="active">Фамилия:</td>
-                                <td>{Data.surname}</td>
+                                <td>Фамилия:</td>
+                                <td><input type="text" className="input" id="surname"  value={this.state.user.surname} disabled={this.state.formDisabled_True}/></td>
                             </tr>
                             <tr>
-                                <td className="active">Имя:</td>
-                                <td>{Data.name}</td>
+                                <td>Имя:</td>
+                                <td><input type="text" className="input" id="name" value={this.state.user.name} disabled={this.state.formDisabled_True}/></td>
                             </tr>
                             <tr>
-                                <td className="active">Отчество:</td>
-                                <td>{Data.patronymic}</td>
+                                <td>Отчество:</td>
+                                <td><input type="text" className="input" id="patronymic" value={this.state.user.patronymic} disabled={this.state.formDisabled_True}/></td>
                             </tr>
                             <tr>
-                                <td className="active">E-mail:</td>
-                                <td>{Data.email}</td>
+                                <td>E-mail:</td>
+                                <td><input type="text" className="input" id="email" onChange={this.handleInput} value={this.state.user.email} disabled={this.state.formDisabled}/></td>
                             </tr>
                             <tr>
-                                <td className="active">Страна:</td>
-                                <td>{Data.country}</td>
+                                <td>Страна:</td>
+                                <td><input type="text" className="input" id="country" onChange={this.handleInput} value={this.state.user.country} disabled={this.state.formDisabled}/></td>
                             </tr>
                             <tr>
-                                <td className="active">Город:</td>
-                                <td>{Data.city}</td>
+                                <td>Город:</td>
+                                <td><input type="text" className="input" id="city" onChange={this.handleInput} value={this.state.user.city} disabled={this.state.formDisabled}/></td>
                             </tr>
-
                             <tr>
-                                <td className="active">Телефон:</td>
-                                <td>{Data.contact}</td>
+                                <td>Телефон:</td>
+                                <td><input type="text" className="input" id="phoneNumber" onChange={this.handleInput} value={this.state.user.phoneNumber} disabled={this.state.formDisabled}/></td>
                             </tr>
 
                             </tbody>
                         </table>
                     </div>
-                    <input type="submit" name="submit" className="button" value="Редактировать"/>
-                    <h4 className="text-center">Ваша история заказов</h4>
+                    <button className="button" id="change" onClick={this.setEditable} disabled={!this.state.formDisabled}>Редактировать</button>
+                    <button className="button save" id="save" onClick={this.updateUserInfo} disabled ={this.state.formDisabled}>Сохранить</button>
+                    <h4>Ваша история заказов</h4>
                     <div className=" history-orders">
-                        <table className=" table_blur">
+                        <table>
                             <tbody>
                             <tr>
                                 <td>Продукт</td>
@@ -123,7 +163,6 @@ export default class UserPage extends Component {
                                 <td>Дата получения</td>
                             </tr>
                             {Data.history.map((item) => <UserItem item={item}/>)}
-
 
                             </tbody>
                         </table>
