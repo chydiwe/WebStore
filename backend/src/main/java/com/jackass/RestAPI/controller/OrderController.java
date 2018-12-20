fd9c383f40cabe4c23be49d04727c3b611c86343
Well <wellusha@gmail.com>
2018-12-20 18:26:50 +0300

    Added CORS

--- backend/src/main/java/com/jackass/RestAPI/controller/OrderController.java
+++ backend/src/main/java/com/jackass/RestAPI/controller/OrderController.java
@@ -5,17 +5,14 @@ import com.jackass.RestAPI.exception.NotFoundException;
 import com.jackass.RestAPI.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.Set;
 
 @RestController
 @RequestMapping("/api/orders")
-//@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:3000")
 public class OrderController {
     @Autowired
     private OrderRepository orderRepository;
